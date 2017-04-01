16f46c01fe0d2e73f69765dbb942166f5a12c458
Eugene Rudoy <gene.devel@gmail.com>
2017-04-01 21:54:53 +0200

    add "modified by"-lines as requested by the author of the original code

--- avm_kernel_config/extract_avm_kernel_config.c
+++ avm_kernel_config/extract_avm_kernel_config.c
@@ -3,6 +3,7 @@
  *                                                                     *
  *                                                                     *
  * Copyright (C) 2016-2017 P.Hämmerlein (http://www.yourfritz.de)      *
+ * Modified by Eugene Rudoy (https://github.com/er13)                  *
  *                                                                     *
  * This program is free software; you can redistribute it and/or       *
  * modify it under the terms of the GNU General Public License         *
